100c9a3c4d82fdd18cdc0cc4c085dffe8866f29e
DaymianDark <nightkonn@gmail.com>
2022-09-22 15:19:23 +0300

    work in progress

--- src/test/java/com/berezkindv/mailRuTests/TestData.java
+++ src/test/java/com/berezkindv/mailRuTests/TestData.java
@@ -2,6 +2,8 @@ package com.berezkindv.mailRuTests;
 
 public class TestData {
 
-//    static String userLogin = "";
-//    static String userPassword = "";
+    static String userLogin = "johnsmithtests";
+    static String userPassword = "jstestspassword61";
+    static String userName = "john";
+    static String userSoname = "smith";
 }
--- src/test/java/com/berezkindv/mailRuTests/Tests.java
+++ src/test/java/com/berezkindv/mailRuTests/Tests.java
@@ -1,8 +1,8 @@
 package com.berezkindv.mailRuTests;
 
-import com.codeborne.selenide.conditions.Text;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -11,10 +11,13 @@ public class Tests {
     @Test
     void loginTest() {
         open("https://e.mail.ru/inbox");
-        $("input[name=username]").setValue("johnsmith");
+        $("input[name=username]").setValue("johnsmithtests");
         $(".submit-button-wrap").$(byText("Ввести пароль")).click();
         $("input[name=password]").setValue("jstestspassword61");
         $(".submit-button-wrap").$(byText("Войти")).click();
+
+        $("div[data-testid='whiteline-account']").click();
+        $(".ph-sidebar").shouldHave(text("John Smith"));
 
         sleep(2000);
     }
